7790af29343c871dab3c2f34ab738e33ea0f2908
Andreas Stocker <shadowstep7@gmail.com>
2016-03-25 23:04:26 +0100

    remove support for chunked header buffers

--- examples/simple_server.rs
+++ examples/simple_server.rs
@@ -1,3 +1,34 @@
+extern crate http2;
+
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::{Read, Write};
+use std::str;
+
+use http2::request::Request;
+
+fn handle_client<'a>(mut stream: TcpStream) {
+	let mut buffer = [0u8; 1000];
+	stream.read(&mut buffer).unwrap();
+	let buffer_text = str::from_utf8(&buffer).unwrap();
+
+	println!("{}", buffer_text);
+
+    let request = Request::from_str(&buffer_text).unwrap();
+
+	stream.write(format!("got path: {}", request.url).as_bytes()).unwrap();
+}
+
 fn main() {
-	println!("todo - start simple server");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    println!("Started server on port 8080");
+
+    for stream in listener.incoming() {
+        thread::spawn(|| {
+            handle_client(stream.unwrap());
+        });
+    }
+
+    println!("todo - start simple server");
 }
--- src/request.rs
+++ src/request.rs
@@ -1,10 +1,7 @@
 use std::collections::HashMap;
-use std::io::Read;
 use std::io;
 use std::str;
 use std::u8;
-
-const BUFFER_SIZE: usize = 8192; // 8 KB - the maximum header size
 
 #[derive(Eq, PartialEq, Debug)]
 pub struct HttpVersion {
@@ -32,65 +29,11 @@ pub enum ParserError {
     InvalidHttpVersion,
 }
 
-#[derive(Eq, PartialEq, Debug)]
-pub enum ParserState<T> {
-    /// The parser does not yet have what it needs in the buffer
-    FillingBuffer,
-    Done(T),
-}
-
-/// Waits until initial line and headers have been received then parses everything.
-/// It does not handle requests with a body. It simply stops after the headers are done.
-pub struct RequestParser {
-    buffer_position: usize,
-    buffer: [u8; BUFFER_SIZE],
-}
-
-impl RequestParser {
-    pub fn new() -> Self {
-        RequestParser {
-            buffer_position: 0,
-            buffer: [0; BUFFER_SIZE],
-        }
-    }
-
-    pub fn read_stream<'a, T: Read>(&'a mut self,
-                                    mut stream: T)
-                                    -> Result<ParserState<Request<'a>>, ParserError> {
-
-        // We borrow a part of the buffer inside this block so it will go out of scope and whole buffer will be available
-        let done = {
-            // Borrow the remaining empty section of the buffer as a slice
-            let mut remaining_buffer = &mut self.buffer[self.buffer_position..];
-
-            let chars_read = try!(stream.read(remaining_buffer)
-                                        .map_err(|err| ParserError::IOError(err)));
-
-            self.buffer_position += chars_read;
-
-            let additional_content = try!(str::from_utf8(&remaining_buffer[..chars_read])
-                                              .map_err(|err| ParserError::Uft8Error(err)));
-
-            // Does the newly parsed content contain an empty line? That means all headers have been sent.
-            additional_content.contains("\r\n\r\n") || additional_content.contains("\n\n")
-        };
-
-        if done {
-            let complete_buffer = try!(str::from_utf8(&self.buffer[..self.buffer_position])
-                                           .map_err(|err| ParserError::Uft8Error(err)));
-
-            let request = try!(RequestParser::parse(&complete_buffer));
-            Ok(ParserState::Done(request))
-        } else {
-            // The empty line is not there so the server is still sending headers
-            Ok(ParserState::FillingBuffer)
-        }
-    }
-
-    fn parse<'a>(content: &'a str) -> Result<Request<'a>, ParserError> {
+impl<'b> Request<'b> {
+    pub fn from_str<'a>(text: &'a str) -> Result<Request<'a>, ParserError> {
         use self::ParserError::InvalidFormat;
 
-        let mut lines = content.lines();
+        let mut lines = text.lines();
 
         let mut initial_line = try!(lines.next().ok_or(InvalidFormat { line: 1 }))
                                    .split_whitespace();
@@ -98,7 +41,7 @@ impl RequestParser {
         let method = try!(initial_line.next().ok_or(InvalidFormat { line: 1 }));
         let url = try!(initial_line.next().ok_or(InvalidFormat { line: 1 }));
         let version_text = try!(initial_line.next().ok_or(InvalidFormat { line: 1 }));
-        let version = try!(RequestParser::parse_version(version_text));
+        let version = try!(Request::parse_version(version_text));
 
         let mut headers = HashMap::new();
 
@@ -117,6 +60,8 @@ impl RequestParser {
 
                 // Header names should always be lowercased
                 headers.insert(name.to_lowercase(), value);
+            } else {
+            	break;
             }
         }
 
@@ -152,58 +97,14 @@ impl RequestParser {
 #[cfg(test)]
 mod tests {
     use super::*;
-	use std::io;
-    use std::io::{Read, Write};
 
     const RAW_REQUEST: &'static str = "GET 	/test/1234  HTTP/1.1\n  Header1 : it\n  Header2:   \
                                        works  \n\n";
 
-    struct MockStream<'a> {
-        request_chunks: [&'a [u8]; 2],
-        chunk_index: usize,
-    }
-
-    impl<'a> MockStream<'a> {
-        fn new(mock_request_text: &'a str) -> MockStream<'a> {
-            let (request_chunk1, request_chunk2) = mock_request_text.as_bytes().split_at(15);
-
-            MockStream {
-                request_chunks: [request_chunk1, request_chunk2],
-                chunk_index: 0,
-            }
-        }
-    }
-
-    impl<'a> Read for MockStream<'a> {
-        fn read(&mut self, mut buffer: &mut [u8]) -> io::Result<usize> {
-            let byte_count = self.request_chunks[self.chunk_index].len();
-            buffer.write(self.request_chunks[self.chunk_index]).unwrap();
-            self.chunk_index += 1;
-
-            Ok(byte_count)
-        }
-    }
-
-    #[test]
-    fn request_parser_buffering() {
-        let mut mock_stream = MockStream::new(RAW_REQUEST);
-
-        let mut parser = RequestParser::new();
-
-        assert_eq!(parser.read_stream(&mut mock_stream).unwrap(),
-                   ParserState::FillingBuffer);
-
-        if let ParserState::Done(request) = parser.read_stream(&mut mock_stream).unwrap() {
-            assert_eq!(request.url, "/test/1234");
-        } else {
-            panic!();
-        }
-    }
-
     #[test]
     fn parse_request() {
 
-        let request = RequestParser::parse(&RAW_REQUEST).unwrap();
+        let request = Request::from_str(&RAW_REQUEST).unwrap();
         // The initial line
         assert_eq!(request.method, "GET");
         assert_eq!(request.url, "/test/1234");
@@ -221,9 +122,9 @@ mod tests {
 
     #[test]
     fn parse_version() {
-        let valid_version = RequestParser::parse_version("HTTP/1.0").unwrap();
-        let invalid_version1 = RequestParser::parse_version("HTTP_1.0");
-        let invalid_version2 = RequestParser::parse_version("HTTP/1");
+        let valid_version = Request::parse_version("HTTP/1.0").unwrap();
+        let invalid_version1 = Request::parse_version("HTTP_1.0");
+        let invalid_version2 = Request::parse_version("HTTP/1");
 
         assert_eq!(valid_version,
                    HttpVersion {
